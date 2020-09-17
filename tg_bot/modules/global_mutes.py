33a7b7afde4a97491aa5e975b127ff26851fb245
Erich Von Daniken <65355700+ErichDanikenOfficial@users.noreply.github.com>
2020-09-17 19:09:03 +0530

    Update global_mutes.py

--- tg_bot/modules/global_mutes.py
+++ tg_bot/modules/global_mutes.py
@@ -64,7 +64,7 @@ def gmute(bot: Bot, update: Update, args: List[str]):
 
         return
 
-    message.reply_text("*      * ðÿ~ ‰")
+    message.reply_text("*                         *")
 
     muter = update.effective_user  # type: Optional[User]
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
